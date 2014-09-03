448f135f8599b0e8d88b373bb4f0b810b8963c1f
Anton Mitrofanov <BugMaster@narod.ru>
2014-09-03 16:35:03 +0400

    Avs2YUV 0.24 BugMaster's mod 2 (2011-9-20)
    merge 2 changes by Oka Motofumi (aka Chikuzen):
      added 128 KB file buffering for speed up
      use ffmpeg for huffyuv encoding instead of mencoder
    added support for interlaced avs sources (it must be field-based instead of frame-based)
    new option: -csp i420/i422/i444 for support of I422/I444 colorspaces output (AviSynth 2.6+ is required)

--- avs2yuv.c
+++ avs2yuv.c
@@ -1,5 +1,8 @@
 // Avs2YUV by Loren Merritt
 
+// Contributors: Anton Mitrofanov (aka BugMaster)
+//               Oka Motofumi (aka Chikuzen)
+
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
@@ -16,8 +19,23 @@
 #define INT_MAX 0x7fffffff
 #endif
 
-#define MY_VERSION "Avs2YUV 0.24"
+#define MY_VERSION "Avs2YUV 0.24bm2"
 #define MAX_FH 10
+#define AVS_BUFSIZE 128*1024
+#define CSP_I420 1
+#define CSP_I422 2
+#define CSP_I444 3
+
+static int csp_to_int(const char *arg)
+{
+    if(!strcasecmp(arg, "i420"))
+        return CSP_I420;
+    if(!strcasecmp(arg, "i422"))
+        return CSP_I422;
+    if(!strcasecmp(arg, "i444"))
+        return CSP_I444;
+    return 0;
+}
 
 int main(int argc, const char* argv[])
 {
@@ -33,11 +51,11 @@ int main(int argc, const char* argv[])
     int end = 0;
     int slave = 0;
     int rawyuv = 0;
-    int frm = -1;
-    int write_target = 0; // how many bytes per frame we expect to write
-    int i;
+    int interlaced = 0;
+    int tff = 0;
+    int csp = CSP_I420;
 
-    for(i=1; i<argc; i++) {
+    for(int i = 1; i < argc; i++) {
         if(argv[i][0] == '-' && argv[i][1] != 0) {
             if(!strcmp(argv[i], "-v"))
                 verbose = 1;
@@ -73,6 +91,16 @@ int main(int argc, const char* argv[])
                 rawyuv = 1;
             } else if(!strcmp(argv[i], "-slave")) {
                 slave = 1;
+            } else if(!strcmp(argv[i], "-csp")) {
+                if(i > argc-2) {
+                    fprintf(stderr, "-csp needs an argument\n");
+                    return 2;
+                }
+                csp = csp_to_int(argv[++i]);
+                if(!csp) {
+                    fprintf(stderr, "-csp '%s' is unknown\n", argv[i]);
+                    return 2;
+                }
             } else {
                 fprintf(stderr, "no such option: %s\n", argv[i]);
                 return 2;
@@ -81,11 +109,11 @@ int main(int argc, const char* argv[])
             infile = argv[i];
             const char *dot = strrchr(infile, '.');
             if(!dot || strcmp(".avs", dot))
-                fprintf(stderr, "infile (%s) doesn't look like an avisynth script\n", infile);
+                fprintf(stderr, "infile '%s' doesn't look like an avisynth script\n", infile);
         } else {
 add_outfile:
             if(out_fhs > MAX_FH-1) {
-                fprintf(stderr, "too many output file\n");
+                fprintf(stderr, "too many output files\n");
                 return 2;
             }
             outfile[out_fhs] = argv[i];
@@ -101,10 +129,11 @@ add_outfile:
         "-seek\tseek to the given frame number\n"
         "-frames\tstop after processing this many frames\n"
         "-slave\tread a list of frame numbers from stdin (one per line)\n"
-        "-raw\toutputs raw I420 instead of yuv4mpeg\n"
+        "-raw\toutput raw I420/I422/I444 instead of yuv4mpeg\n"
+        "-csp\tconvert to I420/I422/I444 colorspace (default I420)\n"
         "The outfile may be \"-\", meaning stdout.\n"
-        "Output format is yuv4mpeg, as used by MPlayer and mjpegtools\n"
-        "Huffyuv output requires MEncoder, and probably doesn't work in Wine.\n"
+        "Output format is yuv4mpeg, as used by MPlayer, FFmpeg, Libav, x264, mjpegtools.\n"
+        "Huffyuv output requires ffmpeg, and probably doesn't work in Wine.\n"
         );
         return 2;
     }
@@ -112,7 +141,7 @@ add_outfile:
     int retval = 1;
     avs_hnd_t avs_h = {0};
     if(internal_avs_load_library(&avs_h) < 0) {
-        fprintf(stderr, "failed to load avisynth.dll\n");
+        fprintf(stderr, "error: failed to load avisynth.dll\n");
         goto fail;
     }
 
@@ -120,7 +149,7 @@ add_outfile:
     if(avs_h.func.avs_get_error) {
         const char *error = avs_h.func.avs_get_error(avs_h.env);
         if(error) {
-            fprintf(stderr, "%s\n", error);
+            fprintf(stderr, "error: %s\n", error);
             goto fail;
         }
     }
@@ -128,7 +157,7 @@ add_outfile:
     AVS_Value arg = avs_new_value_string(infile);
     AVS_Value res = avs_h.func.avs_invoke(avs_h.env, "Import", arg, NULL);
     if(avs_is_error(res)) {
-        fprintf(stderr, "%s\n", avs_as_string(res));
+        fprintf(stderr, "error: %s\n", avs_as_string(res));
         goto fail;
     }
     /* check if the user is using a multi-threaded script and apply distributor if necessary.
@@ -136,23 +165,33 @@ add_outfile:
     AVS_Value mt_test = avs_h.func.avs_invoke(avs_h.env, "GetMTMode", avs_new_value_bool(0), NULL);
     int mt_mode = avs_is_int(mt_test) ? avs_as_int(mt_test) : 0;
     avs_h.func.avs_release_value(mt_test);
-    if( mt_mode > 0 && mt_mode < 5 )
-    {
+    if( mt_mode > 0 && mt_mode < 5 ) {
         AVS_Value temp = avs_h.func.avs_invoke(avs_h.env, "Distributor", res, NULL);
         avs_h.func.avs_release_value(res);
         res = temp;
     }
     if(!avs_is_clip(res)) {
-        fprintf(stderr, "Error: '%s' didn't return a video clip.\n", infile);
+        fprintf(stderr, "error: '%s' didn't return a video clip\n", infile);
         goto fail;
     }
     avs_h.clip = avs_h.func.avs_take_clip(res, avs_h.env);
     const AVS_VideoInfo *inf = avs_h.func.avs_get_video_info(avs_h.clip);
     if(!avs_has_video(inf)) {
-        fprintf(stderr, "Error: '%s' has no video data\n", infile);
+        fprintf(stderr, "error: '%s' has no video data\n", infile);
         goto fail;
     }
-
+    /* if the clip is made of fields instead of frames, call weave to make them frames */
+    if(avs_is_field_based(inf)) {
+        fprintf(stderr, "detected fieldbased (separated) input, weaving to frames\n");
+        AVS_Value tmp = avs_h.func.avs_invoke(avs_h.env, "Weave", res, NULL);
+        if(avs_is_error(tmp)) {
+            fprintf(stderr, "error: couldn't weave fields into frames\n");
+            goto fail;
+        }
+        res = internal_avs_update_clip(&avs_h, &inf, tmp, res);
+        interlaced = 1;
+        tff = avs_is_tff(inf);
+    }
     fprintf(stderr, "%s: %dx%d, ", infile, inf->width, inf->height);
     if(inf->fps_denominator == 1)
         fprintf(stderr, "%d fps, ", inf->fps_numerator);
@@ -160,30 +199,44 @@ add_outfile:
         fprintf(stderr, "%d/%d fps, ", inf->fps_numerator, inf->fps_denominator);
     fprintf(stderr, "%d frames\n", inf->num_frames);
 
-    if(!avs_is_yv12(inf)) {
-        fprintf(stderr, "converting %s -> YV12\n", avs_is_yuy2(inf) ? "YUY2" : avs_is_rgb(inf) ? "RGB" : "?");
-        AVS_Value res2 = avs_h.func.avs_invoke(avs_h.env, "ConvertToYV12", res, NULL);
-        if(avs_is_error(res2)) {
-            fprintf(stderr, "Couldn't convert input clip to YV12\n");
+    if( (csp == CSP_I420 && !avs_is_yv12(inf)) ||
+        (csp == CSP_I422 && !avs_is_yv16(inf)) ||
+        (csp == CSP_I444 && !avs_is_yv24(inf)) )
+    {
+        const char *csp_name = csp == CSP_I444 ? "YV24" :
+                               csp == CSP_I422 ? "YV16" :
+                               "YV12";
+        fprintf(stderr, "converting input clip to %s\n", csp_name);
+        if(csp < CSP_I444 && (inf->width&1)) {
+            fprintf(stderr, "error: input clip width not divisible by 2 (%dx%d)\n", inf->width, inf->height);
             goto fail;
         }
-        res = internal_avs_update_clip(&avs_h, &inf, res2, res);
-    }
-    if(!avs_is_yv12(inf)) {
-        fprintf(stderr, "Couldn't convert input to YV12\n");
-        goto fail;
-    }
-    if(avs_is_field_based(inf)) {
-        fprintf(stderr, "Needs progressive input\n");
-        goto fail;
+        if(csp == CSP_I420 && interlaced && (inf->height&3)) {
+            fprintf(stderr, "error: input clip height not divisible by 4 (%dx%d)\n", inf->width, inf->height);
+            goto fail;
+        }
+        if((csp == CSP_I420 || interlaced) && (inf->height&1)) {
+            fprintf(stderr, "error: input clip height not divisible by 2 (%dx%d)\n", inf->width, inf->height);
+            goto fail;
+        }
+        const char *arg_name[2] = {NULL, "interlaced"};
+        AVS_Value arg_arr[2] = {res, avs_new_value_bool(interlaced)};
+        char conv_func[14] = {"ConvertTo"};
+        strcat(conv_func, csp_name);
+        AVS_Value tmp = avs_h.func.avs_invoke(avs_h.env, conv_func, avs_new_value_array(arg_arr, 2), arg_name);
+        if(avs_is_error(tmp)) {
+            fprintf(stderr, "error: couldn't convert input clip to %s\n", csp_name);
+            goto fail;
+        }
+        res = internal_avs_update_clip(&avs_h, &inf, tmp, res);
     }
     avs_h.func.avs_release_value(res);
 
-    for(i=0; i<out_fhs; i++) {
+    for(int i = 0; i < out_fhs; i++) {
         if(!strcmp(outfile[i], "-")) {
-            for(int j=0; j<i; j++)
+            for(int j = 0; j < i; j++)
                 if(out_fh[j] == stdout) {
-                    fprintf(stderr, "can't write to stdout multiple times\n");
+                    fprintf(stderr, "error: can't write to stdout multiple times\n");
                     goto fail;
                 }
             int dupout = _dup(_fileno(stdout));
@@ -193,7 +246,7 @@ add_outfile:
         } else {
             out_fh[i] = fopen(outfile[i], "wb");
             if(!out_fh[i]) {
-                fprintf(stderr, "fopen(\"%s\") failed", outfile[i]);
+                fprintf(stderr, "error: failed to create/open '%s'\n", outfile[i]);
                 goto fail;
             }
         }
@@ -201,29 +254,55 @@ add_outfile:
     if(hfyufile) {
         char *cmd = malloc(100+strlen(hfyufile));
         if(!cmd) {
-           fprintf(stderr, "malloc failed");
+           fprintf(stderr, "error: malloc failed\n");
            goto fail;
         }
-        sprintf(cmd, "mencoder - -o \"%s\" -quiet -ovc lavc -lavcopts vcodec=ffvhuff:vstrict=-1:pred=2:context=1", hfyufile);
+        sprintf(cmd, "ffmpeg -loglevel quiet -v 0 -y -f yuv4mpegpipe -i - -vcodec ffvhuff -an -f avi \"%s\"", hfyufile);
         out_fh[out_fhs] = _popen(cmd, "wb");
         free(cmd);
         if(!out_fh[out_fhs]) {
-            fprintf(stderr, "failed to exec mencoder\n");
+            fprintf(stderr, "error: failed to exec ffmpeg\n");
             goto fail;
         }
         y4m_headers[out_fhs] = 1;
         out_fhs++;
     }
 
-    for(i=0; i<out_fhs; i++) {
+    char *interlace_type = interlaced ? tff ? "t" : "b" : "p";
+    char *csp_type = NULL;
+    int chroma_h_shift = 0;
+    int chroma_v_shift = 0;
+    switch(csp) {
+        case CSP_I420:
+            csp_type = "420mpeg2";
+            chroma_h_shift = 1;
+            chroma_v_shift = 1;
+            break;
+        case CSP_I422:
+            csp_type = "422";
+            chroma_h_shift = 1;
+            break;
+        case CSP_I444:
+            csp_type = "444";
+            break;
+        default:
+            goto fail; //can't happen
+    }
+    for(int i = 0; i < out_fhs; i++) {
+        if(setvbuf(out_fh[i], NULL, _IOFBF, AVS_BUFSIZE))
+        {
+            fprintf(stderr, "error: failed to create buffer for '%s'\n", outfile[i]);
+            goto fail;
+        }
         if(!y4m_headers[i])
             continue;
-        fprintf(out_fh[i], "YUV4MPEG2 W%d H%d F%u:%u Ip A0:0\n",
-            inf->width, inf->height, inf->fps_numerator, inf->fps_denominator);
+        fprintf(out_fh[i], "YUV4MPEG2 W%d H%d F%u:%u I%s A0:0 C%s\n",
+            inf->width, inf->height, inf->fps_numerator, inf->fps_denominator, interlace_type, csp_type);
         fflush(out_fh[i]);
     }
 
-    write_target = out_fhs*inf->width*inf->height*3/2;
+    int frame_size = inf->width * inf->height + 2 * (inf->width >> chroma_h_shift) * (inf->height >>chroma_v_shift);
+    int write_target = out_fhs * frame_size; // how many bytes per frame we expect to write
 
     if(slave) {
         seek = 0;
@@ -234,7 +313,7 @@ add_outfile:
             end = inf->num_frames;
     }
 
-    for(frm = seek; frm < end; ++frm) {
+    for(int frm = seek; frm < end; ++frm) {
         if(slave) {
             char input[80];
             frm = -1;
@@ -247,10 +326,10 @@ add_outfile:
                 frm = inf->num_frames-1;
         }
 
-        AVS_VideoFrame *f = avs_h.func.avs_get_frame(avs_h.clip, frm); 
+        AVS_VideoFrame *f = avs_h.func.avs_get_frame(avs_h.clip, frm);
         const char *err = avs_h.func.avs_clip_get_error(avs_h.clip);
         if(err) {
-            fprintf(stderr, "%s occurred while reading frame %d\n", err, frm);
+            fprintf(stderr, "error: %s occurred while reading frame %d\n", err, frm);
             goto fail;
         }
 
@@ -258,28 +337,27 @@ add_outfile:
             static const int planes[] = {AVS_PLANAR_Y, AVS_PLANAR_U, AVS_PLANAR_V};
             int wrote = 0;
 
-            for(i=0; i<out_fhs; i++)
+            for(int i = 0; i < out_fhs; i++)
                 if(y4m_headers[i])
                     fwrite("FRAME\n", 1, 6, out_fh[i]);
 
-            for(int p=0; p<3; p++) {
-                int w = inf->width  >> (p ? 1 : 0);
-                int h = inf->height >> (p ? 1 : 0);
+            for(int p = 0; p < 3; p++) {
+                int w = inf->width  >> (p ? chroma_h_shift : 0);
+                int h = inf->height >> (p ? chroma_v_shift : 0);
                 int pitch = avs_get_pitch_p(f, planes[p]);
                 const BYTE* data = avs_get_read_ptr_p(f, planes[p]);
-                int y;
-                for(y=0; y<h; y++) {
-                    for(i=0; i<out_fhs; i++)
+                for(int y = 0; y < h; y++) {
+                    for(int i = 0; i < out_fhs; i++)
                         wrote += fwrite(data, 1, w, out_fh[i]);
                     data += pitch;
                 }
             }
             if(wrote != write_target) {
-                fprintf(stderr, "Output error: wrote only %d of %d bytes\n", wrote, write_target);
+                fprintf(stderr, "error: wrote only %d of %d bytes\n", wrote, write_target);
                 goto fail;
             }
             if(slave) { // assume timing doesn't matter in other modes
-                for(i=0; i<out_fhs; i++)
+                for(int i = 0; i < out_fhs; i++)
                     fflush(out_fh[i]);
             }
         }
@@ -290,6 +368,9 @@ add_outfile:
         avs_h.func.avs_release_video_frame(f);
     }
 
+    for(int i = 0; i < out_fhs; i++)
+        fflush(out_fh[i]);
+
 close_files:
     retval = 0;
 fail:
@@ -298,7 +379,7 @@ fail:
             _pclose(out_fh[out_fhs-1]);
         out_fhs--;
     }
-    for(i=0; i<out_fhs; i++)
+    for(int i = 0; i < out_fhs; i++)
         if(out_fh[i])
             fclose(out_fh[i]);
     if(avs_h.library)
