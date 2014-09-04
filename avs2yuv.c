1bad46f87c812a84a62f76c79139ffd4d7278d19
Anton Mitrofanov <BugMaster@narod.ru>
2014-09-04 23:14:32 +0400

    Avs2YUV 0.24 BugMaster's mod 3 (2014-9-4)
    new options:
      -depth  specify input bit depth (default 8),
              >8 expects avisynth 16-bit hack input video
      -fps    overwrite input framerate
      -par    specify pixel aspect ratio
    new supported value AUTO for -csp option which will try to detect input colorspace or convert to I420

--- avs2yuv.c
+++ avs2yuv.c
@@ -19,15 +19,24 @@
 #define INT_MAX 0x7fffffff
 #endif
 
-#define MY_VERSION "Avs2YUV 0.24bm2"
+#ifdef _MSC_VER
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
+#define snprintf _snprintf
+#endif
+
+#define MY_VERSION "Avs2YUV 0.24bm3"
 #define MAX_FH 10
-#define AVS_BUFSIZE 128*1024
+#define AVS_BUFSIZE (128*1024)
+#define CSP_AUTO (-1)
 #define CSP_I420 1
 #define CSP_I422 2
 #define CSP_I444 3
 
 static int csp_to_int(const char *arg)
 {
+    if(!strcasecmp(arg, "auto"))
+        return CSP_AUTO;
     if(!strcasecmp(arg, "i420"))
         return CSP_I420;
     if(!strcasecmp(arg, "i422"))
@@ -54,6 +63,13 @@ int main(int argc, const char* argv[])
     int interlaced = 0;
     int tff = 0;
     int csp = CSP_I420;
+    int input_depth = 8;
+    int input_width;
+    int input_height;
+    unsigned fps_num = 0;
+    unsigned fps_den = 0;
+    unsigned par_width = 0;
+    unsigned par_height = 0;
 
     for(int i = 1; i < argc; i++) {
         if(argv[i][0] == '-' && argv[i][1] != 0) {
@@ -98,7 +114,52 @@ int main(int argc, const char* argv[])
                 }
                 csp = csp_to_int(argv[++i]);
                 if(!csp) {
-                    fprintf(stderr, "-csp '%s' is unknown\n", argv[i]);
+                    fprintf(stderr, "-csp \"%s\" is unknown\n", argv[i]);
+                    return 2;
+                }
+            } else if(!strcmp(argv[i], "-depth")) {
+                if(i > argc-2) {
+                    fprintf(stderr, "-depth needs an argument\n");
+                    return 2;
+                }
+                input_depth = atoi(argv[++i]);
+                if(input_depth < 8 || input_depth > 16) {
+                    fprintf(stderr, "-depth \"%s\" is not supported\n", argv[i]);
+                    return 2;
+                }
+            } else if(!strcmp(argv[i], "-fps")) {
+                if(i > argc-2) {
+                    fprintf(stderr, "-fps needs an argument\n");
+                    return 2;
+                }
+                const char *str = argv[++i];
+                if(sscanf(str, "%u/%u", &fps_num, &fps_den) != 2) {
+                    char *str_end;
+                    double fps = strtod(str, &str_end);
+                    if(str_end == str || *str_end != '\0' || fps <= 0 || fps > INT_MAX)
+                        fps_num = 0;
+                    else if(fps <= INT_MAX/1000) {
+                        fps_num = (unsigned)(fps * 1000 + .5);
+                        fps_den = 1000;
+                    } else {
+                        fps_num = (unsigned)(fps + .5);
+                        fps_den = 1;
+                    }
+                }
+                if(!fps_num || !fps_den) {
+                    fprintf(stderr, "-fps \"%s\" is not supported\n", str);
+                    return 2;
+                }
+            } else if(!strcmp(argv[i], "-par")) {
+                if(i > argc-2) {
+                    fprintf(stderr, "-par needs an argument\n");
+                    return 2;
+                }
+                const char *str = argv[++i];
+                if( (sscanf(str, "%u:%u", &par_width, &par_height) != 2) &&
+                    (sscanf(str, "%u/%u", &par_width, &par_height) != 2) )
+                {
+                    fprintf(stderr, "-par \"%s\" is not supported\n", str);
                     return 2;
                 }
             } else {
@@ -109,7 +170,7 @@ int main(int argc, const char* argv[])
             infile = argv[i];
             const char *dot = strrchr(infile, '.');
             if(!dot || strcmp(".avs", dot))
-                fprintf(stderr, "infile '%s' doesn't look like an avisynth script\n", infile);
+                fprintf(stderr, "infile \"%s\" doesn't look like an avisynth script\n", infile);
         } else {
 add_outfile:
             if(out_fhs > MAX_FH-1) {
@@ -130,7 +191,10 @@ add_outfile:
         "-frames\tstop after processing this many frames\n"
         "-slave\tread a list of frame numbers from stdin (one per line)\n"
         "-raw\toutput raw I420/I422/I444 instead of yuv4mpeg\n"
-        "-csp\tconvert to I420/I422/I444 colorspace (default I420)\n"
+        "-csp\tconvert to I420/I422/I444 or AUTO colorspace (default I420)\n"
+        "-depth\tspecify input bit depth (default 8)\n"
+        "-fps\toverwrite input framerate\n"
+        "-par\tspecify pixel aspect ratio\n"
         "The outfile may be \"-\", meaning stdout.\n"
         "Output format is yuv4mpeg, as used by MPlayer, FFmpeg, Libav, x264, mjpegtools.\n"
         "Huffyuv output requires ffmpeg, and probably doesn't work in Wine.\n"
@@ -171,13 +235,13 @@ add_outfile:
         res = temp;
     }
     if(!avs_is_clip(res)) {
-        fprintf(stderr, "error: '%s' didn't return a video clip\n", infile);
+        fprintf(stderr, "error: \"%s\" didn't return a video clip\n", infile);
         goto fail;
     }
     avs_h.clip = avs_h.func.avs_take_clip(res, avs_h.env);
     const AVS_VideoInfo *inf = avs_h.func.avs_get_video_info(avs_h.clip);
     if(!avs_has_video(inf)) {
-        fprintf(stderr, "error: '%s' has no video data\n", infile);
+        fprintf(stderr, "error: \"%s\" has no video data\n", infile);
         goto fail;
     }
     /* if the clip is made of fields instead of frames, call weave to make them frames */
@@ -192,17 +256,48 @@ add_outfile:
         interlaced = 1;
         tff = avs_is_tff(inf);
     }
-    fprintf(stderr, "%s: %dx%d, ", infile, inf->width, inf->height);
-    if(inf->fps_denominator == 1)
-        fprintf(stderr, "%d fps, ", inf->fps_numerator);
+
+    input_width  = inf->width;
+    input_height = inf->height;
+    if(input_depth > 8) {
+        if(input_width & 3) {
+            fprintf(stderr, "avisynth 16-bit hack requires that width is at least mod4\n");
+            goto fail;
+        }
+        fprintf(stderr, "avisynth 16-bit hack enabled\n");
+        input_width >>= 1;
+    }
+    if(!fps_num || !fps_den) {
+        fps_num = inf->fps_numerator;
+        fps_den = inf->fps_denominator;
+    }
+
+    fprintf(stderr, "%s: %dx%d, ", infile, input_width, input_height);
+    if(fps_den == 1)
+        fprintf(stderr, "%u fps, ", fps_num);
     else
-        fprintf(stderr, "%d/%d fps, ", inf->fps_numerator, inf->fps_denominator);
+        fprintf(stderr, "%u/%u fps, ", fps_num, fps_den);
     fprintf(stderr, "%d frames\n", inf->num_frames);
+
+    if(csp == CSP_AUTO) {
+        if(avs_is_yv12(inf))
+            csp = CSP_I420;
+        else if(avs_is_yv16(inf) || avs_is_yuy2(inf))
+            csp = CSP_I422;
+        else if(avs_is_yv24(inf))
+            csp = CSP_I444;
+        else
+            csp = CSP_I420; // not supported colorspaces (like RGB) we try convert to I420
+    }
 
     if( (csp == CSP_I420 && !avs_is_yv12(inf)) ||
         (csp == CSP_I422 && !avs_is_yv16(inf)) ||
         (csp == CSP_I444 && !avs_is_yv24(inf)) )
     {
+        if(input_depth > 8) {
+            fprintf(stderr, "error: colorspace conversion is not possible with high depth input [%d-bit depth]\n", input_depth);
+            goto fail;
+        }
         const char *csp_name = csp == CSP_I444 ? "YV24" :
                                csp == CSP_I422 ? "YV16" :
                                "YV12";
@@ -220,7 +315,9 @@ add_outfile:
             goto fail;
         }
         const char *arg_name[2] = {NULL, "interlaced"};
-        AVS_Value arg_arr[2] = {res, avs_new_value_bool(interlaced)};
+        AVS_Value arg_arr[2];
+        arg_arr[0] = res;
+        arg_arr[1] = avs_new_value_bool(interlaced);
         char conv_func[14] = {"ConvertTo"};
         strcat(conv_func, csp_name);
         AVS_Value tmp = avs_h.func.avs_invoke(avs_h.env, conv_func, avs_new_value_array(arg_arr, 2), arg_name);
@@ -246,7 +343,7 @@ add_outfile:
         } else {
             out_fh[i] = fopen(outfile[i], "wb");
             if(!out_fh[i]) {
-                fprintf(stderr, "error: failed to create/open '%s'\n", outfile[i]);
+                fprintf(stderr, "error: failed to create/open \"%s\"\n", outfile[i]);
                 goto fail;
             }
         }
@@ -269,21 +366,30 @@ add_outfile:
     }
 
     char *interlace_type = interlaced ? tff ? "t" : "b" : "p";
-    char *csp_type = NULL;
+    char csp_type[200];
     int chroma_h_shift = 0;
     int chroma_v_shift = 0;
     switch(csp) {
         case CSP_I420:
-            csp_type = "420mpeg2";
+            if(input_depth > 8)
+                sprintf(csp_type, "C420p%d XYSCSS=420P%d", input_depth, input_depth);
+            else
+                strcpy(csp_type, "C420mpeg2 XYSCSS=420MPEG2");
             chroma_h_shift = 1;
             chroma_v_shift = 1;
             break;
         case CSP_I422:
-            csp_type = "422";
+            if(input_depth > 8)
+                sprintf(csp_type, "C422p%d XYSCSS=422P%d", input_depth, input_depth);
+            else
+                strcpy(csp_type, "C422 XYSCSS=422");
             chroma_h_shift = 1;
             break;
         case CSP_I444:
-            csp_type = "444";
+            if(input_depth > 8)
+                sprintf(csp_type, "C444p%d XYSCSS=444P%d", input_depth, input_depth);
+            else
+                strcpy(csp_type, "C444 XYSCSS=444");
             break;
         default:
             goto fail; //can't happen
@@ -291,17 +397,17 @@ add_outfile:
     for(int i = 0; i < out_fhs; i++) {
         if(setvbuf(out_fh[i], NULL, _IOFBF, AVS_BUFSIZE))
         {
-            fprintf(stderr, "error: failed to create buffer for '%s'\n", outfile[i]);
+            fprintf(stderr, "error: failed to create buffer for \"%s\"\n", outfile[i]);
             goto fail;
         }
         if(!y4m_headers[i])
             continue;
-        fprintf(out_fh[i], "YUV4MPEG2 W%d H%d F%u:%u I%s A0:0 C%s\n",
-            inf->width, inf->height, inf->fps_numerator, inf->fps_denominator, interlace_type, csp_type);
+        fprintf(out_fh[i], "YUV4MPEG2 W%d H%d F%u:%u I%s A%u:%u %s\n",
+            input_width, input_height, fps_num, fps_den, interlace_type, par_width, par_height, csp_type);
         fflush(out_fh[i]);
     }
 
-    int frame_size = inf->width * inf->height + 2 * (inf->width >> chroma_h_shift) * (inf->height >>chroma_v_shift);
+    int frame_size = inf->width * inf->height + 2 * (inf->width >> chroma_h_shift) * (inf->height >> chroma_v_shift);
     int write_target = out_fhs * frame_size; // how many bytes per frame we expect to write
 
     if(slave) {
--- compile64.cmd
+++ compile64.cmd
@@ -1,1 +1,1 @@
-x86_64-pc-mingw32-gcc avs2yuv.c -o avs2yuv64.exe -O3 -ffast-math -Wall -Wshadow -Wempty-body -I. -std=gnu99 -fomit-frame-pointer -s -fno-tree-vectorize -fno-zero-initialized-in-bss
+x86_64-w64-mingw32-gcc -m64 avs2yuv.c -o avs2yuv64.exe -O3 -ffast-math -Wall -Wshadow -Wempty-body -I. -std=gnu99 -fomit-frame-pointer -s -fno-tree-vectorize -fno-zero-initialized-in-bss
